a94366f27f52ae7c74df33781e9ca614ab37b557
liamnats17 <124303478+liamnats17@users.noreply.github.com>
2025-07-20 21:16:00 -0400

    Add files via upload

---
+++ cpsc2150/extendedCheckers/views/CheckersFE.java
@@ -0,0 +1,22 @@
+package cpsc2150.extendedCheckers.views;
+
+public class CheckersFE
+{
+    /**
+     * Main method to control the overall game flow.
+     *
+     * @param args Command-line arguments (unused)
+     * @pre none
+     * @post Game is initialized, alternates players each turn, and restarts if users agree after win
+     */
+    public static void main(String[] args)
+    {
+        /*
+        Main function. Remember, for project 1 we aren't implementing anything yet.
+
+        You are welcome to add your own helper functions to this file. However, those functions must be given contracts.
+
+        Also, the main typically doesn't need a contract. But you should still write the class contract.
+         */
+    }
+}
