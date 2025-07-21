974f35da68b8e3ba2b5409aa8096b7a857ea6631
liamnats17 <124303478+liamnats17@users.noreply.github.com>
2025-07-20 21:14:58 -0400

    Add files via upload

---
+++ cpsc2150/extendedCheckers/util/DirectionEnum.java
@@ -0,0 +1,12 @@
+package cpsc2150.extendedCheckers.util;
+
+/**
+ * Enumeration designed to represent the 4 directions a piece is allowed to move in. Each possible direction is
+ * represented with an abbreviated cardinal direction.
+ */
+public enum DirectionEnum {
+    NE,
+    NW,
+    SE,
+    SW
+}
